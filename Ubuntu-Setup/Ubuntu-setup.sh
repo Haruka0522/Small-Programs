3e30ba21d51aea10a85c5e47853690ec878a618c
Haruka0522 <harukapc1@gmail.com>
2019-10-15 16:30:15 +0900

    VSCodeのインストール

--- Ubuntu-Setup/Ubuntu-setup.sh
+++ Ubuntu-Setup/Ubuntu-setup.sh
@@ -70,6 +70,15 @@ wget https://www.pjrc.com/teensy/td_147/TeensyduinoInstall.linux64
 chmod +x TeensyduinoInstall.linux64
 ./TeensyduinoInstall.linux64
 
+#VSCode
+sudo apt install curl
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code
+
 #bashからzshに変更
 sudo apt install zsh
 chsh
