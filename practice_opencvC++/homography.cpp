bcd71ec09a498458dfec867d595757ee16d01c2e
haruka.yamamoto <haruka.yamamoto@firstloop-tech.com>
2020-07-31 18:21:19 +0900

    clean up

--- practice_opencvC++/homography.cpp
+++ practice_opencvC++/homography.cpp
@@ -7,9 +7,15 @@
 #include "json11.hpp"
 
 //vectorもcoutできるようにするやつ
-template <class T>std::ostream &operator<<(std::ostream &o,const std::vector<T>&v)
-{o<<"{";for(int i=0;i<(int)v.size();i++)o<<(i>0?", ":"")<<v[i];o<<"}";return o;}
-
+template <class T>
+std::ostream &operator<<(std::ostream &o, const std::vector<T> &v)
+{
+    o << "{";
+    for (int i = 0; i < (int)v.size(); i++)
+        o << (i > 0 ? ", " : "") << v[i];
+    o << "}";
+    return o;
+}
 
 cv::Mat _get_homography_matrix(json11::Json::array homography_data)
 {
@@ -17,24 +23,24 @@ cv::Mat _get_homography_matrix(json11::Json::array homography_data)
     std::vector<cv::Point2f> src_tmp(data_size);
     std::vector<cv::Point2f> dist_tmp(data_size);
 
-    for (int i=0;i<data_size;i++)
+    for (int i = 0; i < data_size; i++)
     {
-        src_tmp.at(i) = cv::Point2f(homography_data.at(i)["src"].array_items().at(0).int_value(),homography_data.at(i)["src"].array_items().at(1).int_value());
-        dist_tmp.at(i) = cv::Point2f(homography_data.at(i)["dist"].array_items().at(0).int_value(),homography_data.at(i)["dist"].array_items().at(1).int_value());
+        src_tmp.at(i) = cv::Point2f(homography_data.at(i)["src"].array_items().at(0).int_value(), homography_data.at(i)["src"].array_items().at(1).int_value());
+        dist_tmp.at(i) = cv::Point2f(homography_data.at(i)["dist"].array_items().at(0).int_value(), homography_data.at(i)["dist"].array_items().at(1).int_value());
     }
 
     cv::Mat src_mat(src_tmp);
     cv::Mat dist_mat(dist_tmp);
     //std::cout << cv::format(src_tmp,cv::Formatter::FMT_PYTHON) << std::endl;
     //std::cout << cv::format(dist_tmp,cv::Formatter::FMT_PYTHON) << std::endl;
-    const cv::Mat H = cv::findHomography(src_tmp,dist_tmp,cv::RANSAC);
+    const cv::Mat H = cv::findHomography(src_tmp, dist_tmp, cv::RANSAC);
 
     return H;
 }
 
-
 /*
-int transform_by_camparam(const cv::Mat pts,
+動作未確認
+int transform_by_camparam(const vector<cv::Point2f> pts,
                               const cv::Mat K,
                               const cv::Mat D,
                               const cv::Mat P)
@@ -59,16 +65,17 @@ cv::Mat transform_by_data(std::vector<cv::Point2f> pts, json11::Json::array homo
     return ret;
 }
 
-
-inline std::string read_txt(std::string const& path)
+inline std::string read_txt(std::string const &path)
 {
     std::ifstream ifs(path);
     std::stringstream buffer;
     buffer << ifs.rdbuf();
     std::string str = buffer.str();
     // UTF-8のBOM付きをチェック
-    if (str.size() >= 3 && str[0] == -17 && str[1] == -69 && str[2] == -65)  return str.substr(3);
-    else  return str;
+    if (str.size() >= 3 && str[0] == -17 && str[1] == -69 && str[2] == -65)
+        return str.substr(3);
+    else
+        return str;
 }
 
 json11::Json::array read_json_obj(std::string path)
@@ -88,12 +95,14 @@ json11::Json::array read_json_obj(std::string path)
 
 int main()
 {
-    json11::Json::array homography_data = read_json_obj("sample.json");
+    { //transform_by_data()のテスト
+        json11::Json::array homography_data = read_json_obj("sample.json");
 
-    //変換する座標
-    std::vector<cv::Point2f> pts(1);
-    pts.at(0) = cv::Point2f(200,200);
+        //変換する座標
+        std::vector<cv::Point2f> pts(1);
+        pts.at(0) = cv::Point2f(200, 200);
 
-    cv::Mat result = transform_by_data(pts, homography_data);
-    std::cout << result << std::endl;
+        cv::Mat result = transform_by_data(pts, homography_data);
+        std::cout << result << std::endl;
+    }
 }
